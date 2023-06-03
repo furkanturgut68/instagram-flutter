b4f8e6d7d384d9d0ae145570e143f534ec1585ac
furkanturgut68 <furkanmehmeturgut@gmail.com>
2023-06-04 01:45:04 +0300

    login method eklendi

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:igcloneapp/resources/storage_methods.dart';
 
 
 class AuthMethods{
@@ -15,14 +16,17 @@ class AuthMethods{
     required String password,
     required String username,
     required String bio,
-    //required Uint8List file,
+    required Uint8List fileUrl,
   }) async {
     String res = "Some error occurred!";
     try {
       if (email.isNotEmpty || password.isNotEmpty || username.isNotEmpty|| bio.isNotEmpty) {
         UserCredential cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);
 
-        //print(cred.user!.uid);
+        //Storage to photo
+        String photoUrl = await StorageImage().uploadImagetoStorage("profilePics", fileUrl, false);
+
+        print(cred.user!.uid);
         // save to database
           _firestore.collection("users").doc(cred.user!.uid).set({
             'email':email,
@@ -31,19 +35,32 @@ class AuthMethods{
             'bio':bio,
             'followers':[],
             'following':[],
+            'photoUrl':photoUrl,
           });
-
-          //  _firestore.collection("users").doc(cred.user!.uid).set({
-          //   'email':email,
-          //   'uid':cred.user!.uid,
-          //   'username':username,
-          //   'bio':bio,
-          //   'followers':[],
-          //   'following':[],
-          // });
 
           res = "succes";
       }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+
+  //login 
+  Future<String> loginUser({ 
+    required String emails,
+    required String passwords,
+  }) async {
+      String res = "Some error occurred";
+    try {
+      if (emails.isNotEmpty || passwords.isNotEmpty) {
+        await _auth.signInWithEmailAndPassword(email: emails, password: passwords);
+        res = "succes";
+
+      }else {
+        res = "The field error!";
+      }
+      
     } catch (err) {
       res = err.toString();
     }
